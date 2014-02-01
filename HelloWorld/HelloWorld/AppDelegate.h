92d7e8677ccc3fdb732c0ac68844063c5da76551
Daniel Arndt Alves <progdan@MacBook-Air.local>
2014-02-01 11:08:06 -0200

    Criação do Projeto HelloWorld

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Daniel Arndt Alves on 2/1/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
