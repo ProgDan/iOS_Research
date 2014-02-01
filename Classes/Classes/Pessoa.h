dc43aa2244dc78fcee1acd2abcae2fecda35a0c8
Daniel Arndt Alves <progdan@MacBook-Air.local>
2014-02-01 12:15:55 -0200

    Criação da classe Pessoa

---
+++ Classes/Classes/Pessoa.h
@@ -0,0 +1,13 @@
+//
+//  Pessoa.h
+//  Classes
+//
+//  Created by Daniel Arndt Alves on 2/1/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Pessoa : NSObject
+
+@end
---
+++ Classes/Classes/Pessoa.m
@@ -0,0 +1,13 @@
+//
+//  Pessoa.m
+//  Classes
+//
+//  Created by Daniel Arndt Alves on 2/1/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import "Pessoa.h"
+
+@implementation Pessoa
+
+@end
