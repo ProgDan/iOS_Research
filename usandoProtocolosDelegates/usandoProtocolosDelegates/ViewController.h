62082d7ee63c46a75ec953073b286837595e779b
Daniel Arndt Alves <progdan@MacBook-Air.local>
2014-02-02 15:52:24 -0200

    Criação de Projeto - Usando Protocolos e Delegates

---
+++ usandoProtocolosDelegates/usandoProtocolosDelegates/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  usandoProtocolosDelegates
+//
+//  Created by Daniel Arndt Alves on 2/2/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
