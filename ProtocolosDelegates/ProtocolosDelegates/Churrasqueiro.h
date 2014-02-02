05d7f20bb4b14849a6b1f8cbba841de110e23899
Daniel Arndt Alves <progdan@MacBook-Air.local>
2014-02-02 14:41:44 -0200

    Criação de um Protocolo

---
+++ ProtocolosDelegates/ProtocolosDelegates/Churrasqueiro.h
@@ -0,0 +1,13 @@
+//
+//  Churrasqueiro.h
+//  ProtocolosDelegates
+//
+//  Created by Daniel Arndt Alves on 2/2/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol Churrasqueiro <NSObject>
+
+@end
