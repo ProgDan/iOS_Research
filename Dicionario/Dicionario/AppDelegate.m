268a83f38be18be520794098a6faa60e36a1c40c
Daniel Arndt Alves <progdan@MacBook-Air.local>
2014-02-02 11:11:24 -0200

    Dicionário - Adicionando outros tipos de dados

--- Dicionario/Dicionario/AppDelegate.m
+++ Dicionario/Dicionario/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Pessoa.h"
 
 @implementation AppDelegate
 
@@ -40,6 +41,9 @@
     
     NSLog(@"dicionario: %@", dicionario);
     
+    // adicionando outros tipos de dados
+    Pessoa* irmaoVelho = [[Pessoa alloc] initWithNome:@"Eduardo" withIdade:35];
+    [dicionario setObject:irmaoVelho forKey:@"irmaoVelho"];
     
     return YES;
 }
