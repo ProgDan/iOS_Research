ca942ad153ef91dddfd77fd863d39e59d832f7d3
Daniel Arndt Alves <progdan@MacBook-Air.local>
2014-02-08 10:32:04 -0200

    Hello World em iOS

--- HelloWorld_iOS/HelloWorld_iOS/AppDelegate.m
+++ HelloWorld_iOS/HelloWorld_iOS/AppDelegate.m
@@ -16,6 +16,11 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    NSString *string = @"Teste";
+    NSLog(@"%@", string);
+    
+    
     return YES;
 }
 
