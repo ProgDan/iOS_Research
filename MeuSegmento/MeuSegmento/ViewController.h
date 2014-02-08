7f172efbb0299ae6a2e8af1124a7057d3c745e94
Daniel Arndt Alves <progdan@MacBook-Air.local>
2014-02-08 14:33:24 -0200

    Exemplo de Uso - SegmentedControl

---
+++ MeuSegmento/MeuSegmento/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MeuSegmento
+//
+//  Created by Daniel Arndt Alves on 2/8/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MeuSegmento/MeuSegmento/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  MeuSegmento
+//
+//  Created by Daniel Arndt Alves on 2/8/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)mudaImagem:(UISegmentedControl *)sender {
+    switch (sender.selectedSegmentIndex) {
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            break;            
+        default:
+            break;
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
