7f16802bfc5dbd5b4ff687edf782c8862c756177
Daniel Arndt Alves <progdan@MacBook-Air.local>
2014-02-08 11:15:41 -0200

    Criação do Projeto visual - MeuBotao

---
+++ MeuBotao/MeuBotao/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MeuBotao
+//
+//  Created by Daniel Arndt Alves on 2/8/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
