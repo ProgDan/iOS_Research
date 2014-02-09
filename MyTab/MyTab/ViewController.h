1bcd3ca4e3e64b7133c42018fb3c76388327b82e
Daniel Arndt Alves <progdan@MacBook-Air.local>
2014-02-09 17:50:36 -0200

    Exemplo - UITabBarController

---
+++ MyTab/MyTab/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyTab
+//
+//  Created by Daniel Arndt Alves on 2/9/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
