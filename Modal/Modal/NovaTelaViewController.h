b4e42ad6d0563caf87d6fbb5c0640e8d674d8c12
Daniel Arndt Alves <progdan@MacBook-Air.local>
2014-02-09 10:58:01 -0200

    Adicionando tela no history board

---
+++ Modal/Modal/NovaTelaViewController.h
@@ -0,0 +1,13 @@
+//
+//  NovaTelaViewController.h
+//  Modal
+//
+//  Created by Daniel Arndt Alves on 2/9/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NovaTelaViewController : UIViewController
+
+@end
---
+++ Modal/Modal/NovaTelaViewController.m
@@ -0,0 +1,38 @@
+//
+//  NovaTelaViewController.m
+//  Modal
+//
+//  Created by Daniel Arndt Alves on 2/9/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import "NovaTelaViewController.h"
+
+@interface NovaTelaViewController ()
+
+@end
+
+@implementation NovaTelaViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
