497c300b5a31c72d8523cd06eb3e4e614f061ee0
Daniel Arndt Alves <progdan@gmail.com>
2014-02-16 12:16:20 -0300

    Implementação de Busca/Navegação

--- Web/Web/ViewController.m
+++ Web/Web/ViewController.m
@@ -11,6 +11,8 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIWebView *web;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segment;
+@property (weak, nonatomic) IBOutlet UISearchBar *busca;
 
 @end
 
@@ -21,14 +23,29 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
+    [self abreSite:@"http://www.uol.com.br"];
+}
+
+-(void) abreSite:(NSString *)urlString{
+    
+    if ([urlString rangeOfString:@"http://"].location == NSNotFound) {
+        
+        // Formato elegante
+//        urlString = [NSString stringWithFormat:@"http://%@", urlString];
+        
+        // Formato alternativo
+        urlString = [@"http://" stringByAppendingString:urlString];
+    }
+    
     // A URL que será carregada
-    NSURL *url = [NSURL URLWithString:@"http://www.uol.com.br"];
+    NSURL *url = [NSURL URLWithString:urlString];
     
     // Cria uma requisição web
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     
     // Carrega a página usando um request
     [self.web loadRequest:request];
+
 }
 
 - (IBAction)voltar:(UIBarButtonItem *)sender {
@@ -71,6 +88,35 @@
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
 }
 
+- (IBAction)navegaBusca:(UISegmentedControl *)sender {
+    self.busca.text = nil;
+    
+    if (self.segment.selectedSegmentIndex == 0) {
+        self.busca.placeholder = @"Endereço";
+    }
+    else {
+        self.busca.placeholder = @"Google";
+    }
+}
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar{
+    // Opção de Navegar
+    if(self.segment.selectedSegmentIndex == 0){
+        [self abreSite:searchBar.text];
+    }
+    // Opção de Buscar
+    else {
+        NSString *urlGoogle = [NSString stringWithFormat:@"http://www.google.com/search?q=%@",searchBar.text];
+        
+        // Normalização da String para a Web
+        urlGoogle = [urlGoogle stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        
+        [self abreSite:urlGoogle];
+    }
+    
+    [searchBar resignFirstResponder];
+    
+}
 
 - (void)didReceiveMemoryWarning
 {
@@ -78,4 +124,6 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)navegabusca:(UISegmentedControl *)sender {
+}
 @end
