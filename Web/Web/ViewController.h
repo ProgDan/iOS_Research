5eeebf6a91fe999d0418464227a52add247ee202
Daniel Arndt Alves <progdan@gmail.com>
2014-02-16 10:14:36 -0300

    Criação de Projeto - Web View

---
+++ Web/Web/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Web
+//
+//  Created by Daniel Arndt Alves on 2/16/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Web/Web/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Web
+//
+//  Created by Daniel Arndt Alves on 2/16/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
