a6827fe1c74201d84eb1d8a90e397bbd9e17e14d
Daniel Arndt Alves <progdan@gmail.com>
2014-02-16 11:20:05 -0300

    Adicionando botões de ação no browser

--- Web/Web/ViewController.m
+++ Web/Web/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIWebView *web;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 
 @end
 
@@ -21,7 +22,7 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     // A URL que será carregada
-    NSURL *url = [NSURL URLWithString:@"http://www.google.com"];
+    NSURL *url = [NSURL URLWithString:@"http://www.uol.com.br"];
     
     // Cria uma requisição web
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
@@ -29,6 +30,47 @@
     // Carrega a página usando um request
     [self.web loadRequest:request];
 }
+
+- (IBAction)voltar:(UIBarButtonItem *)sender {
+    // Testa se tem conteúdo para voltar
+    if (self.web.canGoBack) {
+        // manda o navegador voltar para a página anterior
+        [self.web goBack];
+    }
+}
+
+- (IBAction)avancar:(UIBarButtonItem *)sender {
+    // Testa se tem conteúdo para avançar
+    if(self.web.canGoForward)
+        // Manda o navegador avançar para a próxima página
+        [self.web goForward];
+}
+
+- (IBAction)reload:(UIBarButtonItem *)sender {
+    [self.web reload];
+    [self.spinner startAnimating];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+}
+
+- (IBAction)stop:(UIBarButtonItem *)sender {
+    if(self.web.isLoading){
+        [self.web stopLoading];
+        [self.spinner stopAnimating];
+        
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    }
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView{
+    [self.spinner startAnimating];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView{
+    [self.spinner stopAnimating];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+}
+
 
 - (void)didReceiveMemoryWarning
 {
