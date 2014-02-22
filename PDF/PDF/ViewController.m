8f5004234cf70c86bfa02414c4fb5bbd43bb64b1
Daniel Arndt Alves <progdan@gmail.com>
2014-02-22 13:40:15 -0300

    Visualizador de Arquivos PDF

--- PDF/PDF/ViewController.m
+++ PDF/PDF/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property(nonatomic, strong) UIDocumentInteractionController *docController;
+
 @end
 
 @implementation ViewController
@@ -20,6 +22,43 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 - (IBAction)abrirPDF:(UIButton *)sender {
+    // Carrega o path para o arquivo
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"iPhone intro" ofType:@"pdf"];
+    
+    // Cria a URL para abertura do arquivo
+    NSURL *url = [NSURL fileURLWithPath:path];
+    
+    // Cria o document controller
+    self.docController = [UIDocumentInteractionController interactionControllerWithURL:url];
+    
+    self.docController.delegate = self;
+    
+    // isso serve para o iPhone sugerir mais aplicações para abrir o seu arquivo
+    self.docController.UTI = @"com.adobe.pdf";
+    
+    [self.docController presentPreviewAnimated:YES];
+}
+- (IBAction)abrirPDFCustomizado:(UIButton *)sender {
+    // Carrega o path para o arquivo
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"iPhone intro" ofType:@"pdf"];
+    
+    // Cria a URL para abertura do arquivo
+    NSURL *url = [NSURL fileURLWithPath:path];
+    
+    // Cria o document controller
+    self.docController = [UIDocumentInteractionController interactionControllerWithURL:url];
+    
+    self.docController.delegate = self;
+    
+    // isso serve para o iPhone sugerir mais aplicações para abrir o seu arquivo
+    self.docController.UTI = @"com.adobe.pdf";
+    
+    [self.docController presentOptionsMenuFromRect:CGRectZero inView:self.view animated:YES];
+}
+
+// Este método informa em qual viewController o documento será visualizado
+- (UIViewController *)documentInteractionControllerViewControllerForPreview:(UIDocumentInteractionController *)controller{
+    return self;
 }
 
 - (void)didReceiveMemoryWarning
