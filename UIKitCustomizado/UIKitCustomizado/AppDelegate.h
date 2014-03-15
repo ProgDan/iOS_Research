5793e639f6907b0851d27812ec56c4d5cdc2da30
Daniel Arndt Alves <progdan@gmail.com>
2014-03-15 17:58:49 -0300

    Customização de Interface

---
+++ UIKitCustomizado/UIKitCustomizado/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIKitCustomizado
+//
+//  Created by Daniel Arndt Alves on 3/15/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIKitCustomizado/UIKitCustomizado/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UIKitCustomizado
+//
+//  Created by Daniel Arndt Alves on 3/15/14.
+//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
