04c7fd092988c74cd972bcd96d72d23a32444883
Daniel Arndt Alves <progdan@gmail.com>
2014-04-22 16:24:23 -0300

    Exemplo - Propagandas no App (iAd)

--- Propaganda/Propaganda/ViewController.h
+++ Propaganda/Propaganda/ViewController.h
@@ -2,8 +2,8 @@
 //  ViewController.h
 //  Propaganda
 //
-//  Created by Daniel Arndt Alves on 3/29/14.
-//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//  Created by Eduardo Lima on 3/29/14.
+//  Copyright (c) 2014 personal. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- Propaganda/Propaganda/ViewController.m
+++ Propaganda/Propaganda/ViewController.m
@@ -2,8 +2,8 @@
 //  ViewController.m
 //  Propaganda
 //
-//  Created by Daniel Arndt Alves on 3/29/14.
-//  Copyright (c) 2014 ProgDan Software. All rights reserved.
+//  Created by Eduardo Lima on 3/29/14.
+//  Copyright (c) 2014 personal. All rights reserved.
 //
 
 #import "ViewController.h"
@@ -20,16 +20,27 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
--(void)bannerViewDidLoadAd:(ADBannerView *)banner
+- (void)didReceiveMemoryWarning
 {
-    banner.hidden = NO;
-    banner.alpha = 1.0;
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
--(void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
-{
-    banner.hidden = YES;
+- (void)bannerViewDidLoadAd:(ADBannerView *)banner{
+    
+    banner.hidden = NO;
+    banner.alpha = 1.0;
+    
+}
+
+-(void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error{
+    
+    NSLog(@"ERROR %@", error.localizedFailureReason);
+    
+    //banner.hidden = YES;
+    
     banner.alpha = 0.0;
+    
 }
 
 @end
