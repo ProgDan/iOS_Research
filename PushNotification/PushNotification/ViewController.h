4e8c5721d7333b93755ba7f6e48ce59115488716
Daniel Arndt Alves <progdan@gmail.com>
2014-04-22 16:25:38 -0300

    Exemplo - Push Notification

---
+++ PushNotification/PushNotification/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PushNotification
+//
+//  Created by Eduardo Lima on 3/29/14.
+//  Copyright (c) 2014 personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PushNotification/PushNotification/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PushNotification
+//
+//  Created by Eduardo Lima on 3/29/14.
+//  Copyright (c) 2014 personal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
